2e48788e68598a16b9f8cd161216fbbcbc52dcf9
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-11 23:51:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //your JS code here. If required.
-let first_element=parseInt(prompt("first element"));
-let second_element=parseInt(prompt("second element"));
-let firstmultiply2=first_element * 2;
-let result=(firstmultiply2+second_element);
-alert(`"The final result is: ${result} "`);
+// // let first_element=parse(prompt("first element"));
+// // let second_element=parseInt(prompt("second element"));
+// let firstmultiply2=first_element * 2;
+// let result=(firstmultiply2+second_element);
+// alert(`"The final result is: ${result} "`);
+
+// / Shortened JavaScript code
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let result = (num1 * 2) + num2;
+alert("The final result is: " + result);
