292510ff0d8fd70ded04aebcc70a2317cb3c391b
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-11 23:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let first_element=parseInt(prompt("first element"));
+let second_element=parseInt(prompt("second element"));
+let firstmultiply2=first_element * 2;
+let result=(firstmultiply2+second_element);
+alert(`"The final result is: ${result} "`);
